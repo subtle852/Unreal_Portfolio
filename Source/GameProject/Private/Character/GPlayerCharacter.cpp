de68746cc22ccd3e6d39c13650758c531f801f57
subtle852 <subtle852@gmail.com>
2024-06-05 11:11:13 +0900

    fix: PlayerCharacter BodyMesh 분리 및 ClothMesh 적용
    Separated 캐릭터 메쉬를 사용해서 Cloth 탈착 가능, 이를 통해 옷과 캐릭 메쉬의 충돌 그리고 이질감을 최소화

--- Source/GameProject/Private/Character/GPlayerCharacter.cpp
+++ Source/GameProject/Private/Character/GPlayerCharacter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:481a816b49dc9acab9656b8f8a71452dabed1eb41c72f4137bff36cc6e6bda48
-size 31032
+oid sha256:f309edbf21d6a2d2b734ce911d47fc8108f37071d9d5fb8bc76c6152c3cf7ff7
+size 33283
--- Source/GameProject/Public/Animation/GAnimInstance.h
+++ Source/GameProject/Public/Animation/GAnimInstance.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bee3ff47ec27a085607a8d86651a080da3d871245acfe8ec64d6f7b502615627
-size 5118
+oid sha256:f92edfa1e003eb9da26f726cdb81225a7e47606d1760b42ee52e2cec836f71fe
+size 5112
